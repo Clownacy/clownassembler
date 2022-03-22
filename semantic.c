09b4fb6e469a19867dd7df7741475f4353a9734f
Clownacy <Clownacy@users.noreply.github.com>
2022-03-22 12:34:12 +0000

    Don't let DuplicateString accept a NULL pointer
    The caller should specifically check if the string it wants duplicating is
    NULL.

--- semantic.c
+++ semantic.c
@@ -208,22 +208,12 @@ static char* DuplicateString(const char *string)
 {
 	char *duplicated_string;
 
-	if (string == NULL)
-	{
-		duplicated_string = malloc(1);
+	const size_t string_size = strlen(string) + 1;
 
-		if (duplicated_string != NULL)
-			duplicated_string[0] = '\0';
-	}
-	else
-	{
-		const size_t string_size = strlen(string) + 1;
+	duplicated_string = malloc(string_size);
 
-		duplicated_string = malloc(string_size);
-
-		if (duplicated_string != NULL)
-			memcpy(duplicated_string, string, string_size);
-	}
+	if (duplicated_string != NULL)
+		memcpy(duplicated_string, string, string_size);
 
 	return duplicated_string;
 }
