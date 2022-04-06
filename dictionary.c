7b22ab11a7a87046d19da3ffe09a5f0883d6ae5e
Clownacy <Clownacy@users.noreply.github.com>
2022-04-06 13:42:21 +0100

    Use binary search tree in dictionary
    This should speed up lookups considerably.

--- dictionary.c
+++ dictionary.c
@@ -9,6 +9,14 @@
 
 /* TODO - Destructors */
 
+typedef enum SearchResult
+{
+	SEARCH_RESULT_FOUND,
+	SEARCH_RESULT_NOT_FOUND_WOULD_BE_LEFT_CHILD,
+	SEARCH_RESULT_NOT_FOUND_WOULD_BE_RIGHT_CHILD,
+	SEARCH_RESULT_BUCKET_EMPTY
+} SearchResult;
+
 /* Like strcasecmp, but doesn't check for null characters. */
 static int memcasecmp(const void *lhs, const void *rhs, size_t count)
 {
@@ -35,7 +43,7 @@ static int memcasecmp(const void *lhs, const void *rhs, size_t count)
 	return delta;
 }
 
-static Dictionary_BucketNode** GetBucket(Dictionary_State *state, const char *identifier, size_t identifier_length)
+static Dictionary_Bucket* GetBucket(Dictionary_State *state, const char *identifier, size_t identifier_length)
 {
 	unsigned int hash;
 	size_t i;
@@ -47,81 +55,236 @@ static Dictionary_BucketNode** GetBucket(Dictionary_State *state, const char *id
 	for (i = 0; i < identifier_length; ++i)
 		hash = hash * 33 + tolower(*identifier++); /* Hash the identifier in lower-case form, so that case-insensitive mode works. */
 
-	return &state->buckets[hash % CC_COUNT_OF(state->buckets)];
+	return &state->hash_table[hash % CC_COUNT_OF(state->hash_table)];
 }
 
-static Dictionary_BucketNode** FindEntry(Dictionary_State *state, const char *identifier, size_t identifier_length)
+static SearchResult SearchBucket(Dictionary_State *state, Dictionary_Bucket *bucket, const char *identifier, size_t identifier_length, Dictionary_Node **node_pointer)
 {
-	Dictionary_BucketNode **node;
+	SearchResult search_result;
 
-	for (node = GetBucket(state, identifier, identifier_length); *node != NULL; node = &(*node)->next)
-		if ((*node)->identifier_length == identifier_length && state->compare_identifiers((*node)->identifier, identifier, identifier_length) == 0)
-			break;
+	if (bucket->binary_search_tree == NULL)
+	{
+		search_result = SEARCH_RESULT_BUCKET_EMPTY;
+	}
+	else
+	{
+		Dictionary_Node *node;
 
-	return node;
+		node = bucket->binary_search_tree;
+
+		for (;;)
+		{
+			int comparison_result;
+
+			if (identifier_length < node->identifier_length)
+				comparison_result = -1;
+			else if (identifier_length > node->identifier_length)
+				comparison_result = 1;
+			else /*if (identifier_length == node->identifier_length)*/
+				comparison_result = state->compare_identifiers(identifier, node->identifier, identifier_length);
+
+			if (comparison_result < 0)
+			{
+				if (node->left_child == NULL)
+				{
+					search_result = SEARCH_RESULT_NOT_FOUND_WOULD_BE_LEFT_CHILD;
+					break;
+				}
+
+				node = node->left_child;
+			}
+			else if (comparison_result > 0)
+			{
+				if (node->right_child == NULL)
+				{
+					search_result = SEARCH_RESULT_NOT_FOUND_WOULD_BE_RIGHT_CHILD;
+					break;
+				}
+
+				node = node->right_child;
+			}
+			else /*if (comparison_result == 0)*/
+			{
+				search_result = SEARCH_RESULT_FOUND;
+				break;
+			}
+		}
+
+		*node_pointer = node;
+	}
+
+	return search_result;
+}
+
+static void RemoveNodeFromBucket(Dictionary_Bucket *bucket, Dictionary_Node *node)
+{
+	Dictionary_Node **binary_search_tree_pointer;
+
+	/* Remove node from binary search tree. */
+	if (node->parent == NULL)
+		binary_search_tree_pointer = &bucket->binary_search_tree;
+	else if (node->parent->left_child == node)
+		binary_search_tree_pointer = &node->parent->left_child;
+	else /*if (node->parent->right_child == node)*/
+		binary_search_tree_pointer = &node->parent->right_child;
+
+	if (node->left_child != NULL && node->right_child != NULL)
+	{
+		/* Find successor. */
+		Dictionary_Node *successor_parent;
+		Dictionary_Node *successor;
+
+		successor_parent = node;
+
+		while (successor_parent->left_child->left_child != NULL)
+			successor_parent = successor_parent->left_child;
+
+		successor = successor_parent->left_child;
+
+		/* Replace the successor with its own right child. */
+		successor_parent->left_child = successor->right_child;
+		successor_parent->left_child->parent = successor_parent;
+
+		/* Give the successor the node's children. */
+		successor->left_child = node->left_child;
+		successor->left_child->parent = successor;
+
+		if (node->right_child != successor)
+		{
+			successor->right_child = node->right_child;
+			successor->right_child->parent = successor;
+		}
+
+		/* Replace the node with its successor. */
+		*binary_search_tree_pointer = successor;
+		successor->parent = node->parent;
+	}
+	else
+	{
+		if (node->left_child != NULL)
+		{
+			/* Replace the node with its left child. */
+			*binary_search_tree_pointer = node->left_child;
+			node->left_child->parent = node->parent;
+		}
+		else if (node->right_child != NULL)
+		{
+			/* Replace the node with its right child. */
+			*binary_search_tree_pointer = node->right_child;
+			node->right_child->parent = node->parent;
+		}
+		else /*if (node->left_child == NULL && node->right_child == NULL)*/
+		{
+			/* Destroy the node. */
+			*binary_search_tree_pointer = NULL;
+		}
+	}
+
+	/* Remove node from linked-list. */
+	if (node->next != NULL)
+		node->next->previous = node->previous;
+
+	if (node->previous != NULL)
+		node->previous->next = node->next;
+	else
+		bucket->linked_list = node->next;
+
+	free(node);
 }
 
 void Dictionary_Init(Dictionary_State *state, cc_bool case_insensitive)
 {
 	size_t i;
 
-	for (i = 0; i < CC_COUNT_OF(state->buckets); ++i)
-		state->buckets[i] = NULL;
+	for (i = 0; i < CC_COUNT_OF(state->hash_table); ++i)
+	{
+		state->hash_table[i].binary_search_tree = NULL;
+		state->hash_table[i].linked_list = NULL;
+	}
 
 	state->compare_identifiers = case_insensitive ? memcasecmp : memcmp;
 }
-
+#include <stdio.h>
 void Dictionary_Deinit(Dictionary_State *state)
 {
 	size_t i;
+	unsigned int total_thing = 0;
 
-	for (i = 0; i < CC_COUNT_OF(state->buckets); ++i)
+	for (i = 0; i < CC_COUNT_OF(state->hash_table); ++i)
 	{
-		Dictionary_BucketNode *node = state->buckets[i];
+		Dictionary_Node *node = state->hash_table[i].linked_list;
 
 		while (node != NULL)
 		{
-			Dictionary_BucketNode *next_node = node->next;
+			Dictionary_Node *next_node = node->next;
 
 			free(node);
 
 			node = next_node;
+			++total_thing;
 		}
 	}
+
+	fprintf(stderr, "Deinited %u nodes\n", total_thing);
 }
 
-cc_bool Dictionary_LookUpAndCreateIfNotExist(Dictionary_State *state, const char *identifier, size_t identifier_length, Dictionary_Entry **entry)
+cc_bool Dictionary_LookUpAndCreateIfNotExist(Dictionary_State *state, const char *identifier, size_t identifier_length, Dictionary_Entry **entry_pointer)
 {
 	cc_bool success = cc_true;
 
-	Dictionary_BucketNode **node_pointer = FindEntry(state, identifier, identifier_length);
+	Dictionary_Node *found_node;
 
-	if (*node_pointer == NULL)
+	Dictionary_Bucket* const bucket = GetBucket(state, identifier, identifier_length);
+	const SearchResult search_result = SearchBucket(state, bucket, identifier, identifier_length, &found_node);
+
+	if (search_result == SEARCH_RESULT_FOUND)
 	{
-		Dictionary_BucketNode *node = malloc(sizeof(Dictionary_BucketNode) + identifier_length);
+		if (entry_pointer != NULL)
+			*entry_pointer = &found_node->entry;
+	}
+	else
+	{
+		Dictionary_Node *new_node = malloc(sizeof(Dictionary_Node) + identifier_length);
 
-		if (node == NULL)
+		if (new_node == NULL)
 		{
 			success = cc_false;
 		}
 		else
 		{
-			node->next = NULL;
-			*node_pointer = node;
+			new_node->next = NULL;
+			new_node->previous = NULL;
+			new_node->parent = NULL;
+			new_node->left_child = NULL;
+			new_node->right_child = NULL;
 
-			node->identifier = (char*)(node + 1);
-			memcpy(node->identifier, identifier, identifier_length);
-			node->identifier_length = identifier_length;
-			node->entry.type = -1;
+			new_node->identifier = (char*)(new_node + 1);
+			memcpy(new_node->identifier, identifier, identifier_length);
+			new_node->identifier_length = identifier_length;
+			new_node->entry.type = -1;
 
-			if (entry != NULL)
-				*entry = &node->entry;
+			if (search_result == SEARCH_RESULT_BUCKET_EMPTY)
+			{
+				bucket->binary_search_tree = new_node;
+			}
+			else
+			{
+				/* Insert node into correct part of binary search tree. */
+				new_node->parent = found_node;
+
+				if (search_result == SEARCH_RESULT_NOT_FOUND_WOULD_BE_LEFT_CHILD)
+					found_node->left_child = new_node;
+				else /*if (search_result == SEARCH_RESULT_NOT_FOUND_WOULD_BE_RIGHT_CHILD)*/
+					found_node->right_child = new_node;
+			}
+
+			/* Insert node at start of current bucket's linked list. */
+			new_node->next = bucket->linked_list;
+			bucket->linked_list = new_node;
+
+			if (entry_pointer != NULL)
+				*entry_pointer = &new_node->entry;
 		}
-	}
-	else
-	{
-		if (entry != NULL)
-			*entry = &(*node_pointer)->entry;
 	}
 
 	return success;
@@ -130,13 +293,14 @@ cc_bool Dictionary_LookUpAndCreateIfNotExist(Dictionary_State *state, const char
 Dictionary_Entry* Dictionary_LookUp(Dictionary_State *state, const char *identifier, size_t identifier_length)
 {
 	Dictionary_Entry *entry;
+	Dictionary_Node *node;
 
-	Dictionary_BucketNode *node_pointer = *FindEntry(state, identifier, identifier_length);
+	Dictionary_Bucket* const bucket = GetBucket(state, identifier, identifier_length);
 
-	if (node_pointer == NULL)
+	if (SearchBucket(state, bucket, identifier, identifier_length, &node) != SEARCH_RESULT_FOUND)
 		entry = NULL;
 	else
-		entry = &node_pointer->entry;
+		entry = &node->entry;
 
 	return entry;
 }
@@ -145,45 +309,42 @@ cc_bool Dictionary_Remove(Dictionary_State *state, const char *identifier, size_
 {
 	cc_bool success = cc_true;
 
-	Dictionary_BucketNode **node_pointer = FindEntry(state, identifier, identifier_length);
+	Dictionary_Node *node;
 
-	if (*node_pointer == NULL)
-	{
+	Dictionary_Bucket* const bucket = GetBucket(state, identifier, identifier_length);
+
+	if (SearchBucket(state, bucket, identifier, identifier_length, &node) != SEARCH_RESULT_FOUND)
 		success = cc_false;
-	}
 	else
-	{
-		Dictionary_BucketNode *next_node = (*node_pointer)->next;
-
-		free(*node_pointer);
-
-		*node_pointer = next_node;
-	}
+		RemoveNodeFromBucket(bucket, node);
 
 	return success;
 }
 
+#include <stdio.h>
 void Dictionary_Filter(Dictionary_State *state, cc_bool (*filter_function)(Dictionary_Entry *entry, const char *identifier, size_t identifier_length, void *user_data), void *user_data)
 {
 	size_t i;
+	unsigned int total_nodes = 0;
 
-	for (i = 0; i < CC_COUNT_OF(state->buckets); ++i)
+	for (i = 0; i < CC_COUNT_OF(state->hash_table); ++i)
 	{
-		Dictionary_BucketNode **node_pointer = &state->buckets[i];
+		Dictionary_Node *node;
 
-		while (*node_pointer != NULL)
+		Dictionary_Bucket* const bucket = &state->hash_table[i];
+
+		node = bucket->linked_list;
+
+		while (node != NULL)
 		{
-			Dictionary_BucketNode *node = *node_pointer;
+			Dictionary_Node* const next_node = node->next;
 
 			if (!filter_function(&node->entry, node->identifier, node->identifier_length, user_data))
-			{
-				*node_pointer = node->next;
-				free(node);
-			}
-			else
-			{
-				node_pointer = &node->next;
-			}
+				RemoveNodeFromBucket(bucket, node);
+
+			node = next_node;
+			++total_nodes;
 		}
 	}
+	fprintf(stderr, "Filtered %u nodes.\n", total_nodes);
 }
--- dictionary.h
+++ dictionary.h
@@ -16,25 +16,36 @@ typedef struct Dictionary_Entry
 	} shared; 
 } Dictionary_Entry;
 
-typedef struct Dictionary_BucketNode
+typedef struct Dictionary_Node
 {
-	struct Dictionary_BucketNode *next;
+	struct Dictionary_Node *next;
+	struct Dictionary_Node *previous;
+
+	struct Dictionary_Node *parent;
+	struct Dictionary_Node *left_child;
+	struct Dictionary_Node *right_child;
 
 	char *identifier;
 	size_t identifier_length;
 	Dictionary_Entry entry;
-} Dictionary_BucketNode;
+} Dictionary_Node;
+
+typedef struct Dictionary_Bucket
+{
+	Dictionary_Node *binary_search_tree;
+	Dictionary_Node *linked_list;
+} Dictionary_Bucket;
 
 typedef struct Dictionary_State
 {
-	Dictionary_BucketNode *buckets[0x100];
+	Dictionary_Bucket hash_table[0x100];
 	int (*compare_identifiers)(const void *lhs, const void *rhs, size_t count);
 } Dictionary_State;
 
 void Dictionary_Init(Dictionary_State *state, cc_bool case_insensitive);
 void Dictionary_Deinit(Dictionary_State *state);
 
-cc_bool Dictionary_LookUpAndCreateIfNotExist(Dictionary_State *state, const char *identifier, size_t identifier_length, Dictionary_Entry **entry);
+cc_bool Dictionary_LookUpAndCreateIfNotExist(Dictionary_State *state, const char *identifier, size_t identifier_length, Dictionary_Entry **entry_pointer);
 Dictionary_Entry* Dictionary_LookUp(Dictionary_State *state, const char *identifier, size_t identifier_length);
 cc_bool Dictionary_Remove(Dictionary_State *state, const char *identifier, size_t identifier_length);
 void Dictionary_Filter(Dictionary_State *state, cc_bool (*filter_function)(Dictionary_Entry *entry, const char *identifier, size_t identifier_length, void *user_data), void *user_data);
