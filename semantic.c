40decf7a056901f7489505244caf6b97d1d1b242
Clownacy <Clownacy@users.noreply.github.com>
2026-05-03 09:06:20 +0100

    Clean-up statement initialisation.

--- semantic.c
+++ semantic.c
@@ -5427,10 +5427,9 @@ static cc_bool ParseStatement(SemanticState* const state, Statement* const state
 {
 	/* Parse the source line with Flex and Bison (Lex and Yacc). */
 	const YY_BUFFER_STATE buffer = m68kasm__scan_bytes(StringView_Data(view), StringView_Length(view), state->flex_state);
-	const Statement empty_statement = {0};
 	int parse_result;
 
-	*statement = empty_statement; /* We need to be able to call DestroyStatement if we fail, even if m68kasm_parse didn't parse shit - without this it would be left uninitialized */
+	statement->type = STATEMENT_TYPE_EMPTY;
 	parse_result = m68kasm_parse(state->flex_state, statement);
 	m68kasm__delete_buffer(buffer, state->flex_state);
 
@@ -5441,7 +5440,6 @@ static cc_bool ParseStatement(SemanticState* const state, Statement* const state
 	if (parse_result == 2)
 		OutOfMemoryError(state);
 
-	/* We need to free `statement`, given that a statement may have been meaningfully formed into it even on error, if e.g. the error was due to trailing garbage on a line */
 	DestroyStatement(statement);
 	return cc_false;
 }
